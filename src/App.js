8261f94a29ce69aaaf05e3ef7f3b8ed9d80baf58
Jeremias <jeremias.pazos@hotmail.com>
2020-10-25 00:29:55 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Main from './components/MainComponent.js';
 import './App.css';
 import {Component} from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
-    render(){ 
+    
+  render(){ 
     return (
+      <BrowserRouter>
       <div>
         <Main/>
       </div>
+      </BrowserRouter>
     );
   }
   
