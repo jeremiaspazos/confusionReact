8cfa49699f4903958ea771c604cfcabcd802d352
Jeremias <jeremias.pazos@hotmail.com>
2020-11-09 21:45:06 -0300

    Redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Main from './components/MainComponent.js';
 import './App.css';
 import {Component} from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore(); 
 class App extends Component {
-    
+  
   render(){ 
     return (
+      <Provider store={store}>
       <BrowserRouter>
       <div>
         <Main/>
       </div>
       </BrowserRouter>
+      </Provider>
     );
   }
   
